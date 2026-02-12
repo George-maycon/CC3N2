9a4eca5e23a54b9e461c20116f00c31dc3d08f64
George-maycon <george2016.rodrigues@gmail.com>
2026-02-12 20:41:19 -0300

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplo1202a;
+
+/**
+ *
+ * @author alunodev13
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        Conta c1 = new Conta();
+        
+
+        
+       c1.creditar(2000);
+    c1.creditar(2000);
+      c1.creditar(2000);
+    c1.creditar(2000);
+     //dificio
+        System.out.println( "valor sa conta e " + c1.getsaldo());
+    }
+}
